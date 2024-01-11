9deb980c99541a4175ff5d85ae8f79e0be2e8a1c
Airah Yusuff <airahyusuff@gmail.com>
2024-01-11 13:56:53 +0000

    setup stack for amplify app

--- cdk-next-app-2/lib/cdk-next-app-2-stack.ts
+++ cdk-next-app-2/lib/cdk-next-app-2-stack.ts
@@ -1,16 +1,57 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  App,
+  GitHubSourceCodeProvider,
+  Platform,
+  RedirectStatus
+} from "@aws-cdk/aws-amplify-alpha";
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 
 export class CdkNextApp2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //create a new Amplify app
+    const amplifyApp = new App(this, 'NextApp', {
+      sourceCodeProvider: new GitHubSourceCodeProvider({
+        owner: 'khairahscorner',
+        repository: 'sample-next-app',
+        oauthToken: cdk.SecretValue.secretsManager('github-token-cdk'),
+      }),
+      autoBranchDeletion: true,
+      platform: Platform.WEB_COMPUTE,
+      customRules: [{
+        source: '/<*>',
+        target: '/index.html',
+        status: RedirectStatus.NOT_FOUND_REWRITE
+      }],
+      buildSpec: codebuild.BuildSpec.fromObjectToYaml({
+          // Alternatively add a `amplify.yml` to the repo
+          version: '1.0',
+          frontend: {
+            phases: {
+              preBuild: {
+                commands: ['npm ci'],
+              },
+              build: {
+                commands: ['npm run build'],
+              },
+            },
+            artifacts: {
+              baseDirectory: '.next',
+              files: ['**/*'],
+            },
+            // save time on npm reinstalling
+            cache: {
+              paths: ['node_modules/**/*'],
+            }
+          }
+      }),
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkNextApp2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    amplifyApp.addBranch('main', {
+      stage: 'PRODUCTION',
+    })
   }
 }
